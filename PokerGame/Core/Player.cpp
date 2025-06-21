55ceae683d8b1ca9bcae5708d4942ab961f725ca
Michał Tracichleb <michal.tracichleb@gmail.com>
2025-06-21 11:03:35 +0200

    Add abstract Player base class

--- PokerGame/Core/Player.cpp
+++ PokerGame/Core/Player.cpp
@@ -1,1 +1,52 @@
 ﻿#include "Player.h"
+
+using namespace std;
+
+Player::Player(string name, const int initialChips)
+    : _name(std::move(name)), _chips(initialChips), _folded(false)
+{
+}
+
+const string& Player::GetName() const {
+    return _name;
+}
+
+int Player::GetChips() const {
+    return _chips;
+}
+
+void Player::AddChips(const int amount) {
+    _chips += amount;
+}
+
+bool Player::RemoveChips(const int amount) {
+    if (_chips >= amount) {
+        _chips -= amount;
+        return true;
+    }
+    return false;
+}
+
+void Player::AddCard(const Card& card) {
+    _hand.push_back(card);
+}
+
+void Player::ClearHand() {
+    _hand.clear();
+}
+
+const vector<Card>& Player::GetHand() const {
+    return _hand;
+}
+
+bool Player::IsFolded() const {
+    return _folded;
+}
+
+void Player::Fold() {
+    _folded = true;
+}
+
+void Player::Unfold() {
+    _folded = false;
+}
--- PokerGame/Core/Player.h
+++ PokerGame/Core/Player.h
@@ -1,7 +1,43 @@
 ﻿#pragma once
 
+#include <vector>
+#include <string>
+#include "Card.h"
+
+enum class PlayerDecision : uint8_t{
+    Fold,
+    Check,
+    Call,
+    Bet,
+    Raise,
+
+    Size // Only for iteration
+};
+
 class Player
 {
 public:
-    
+    Player(std::string name, const int initialChips);
+    virtual ~Player() = default;
+
+    const std::string& GetName() const;
+    int GetChips() const;
+    void AddChips(int amount);
+    bool RemoveChips(int amount);
+
+    void AddCard(const Card& card);
+    void ClearHand();
+    const std::vector<Card>& GetHand() const;
+
+    bool IsFolded() const;
+    void Fold();
+    void Unfold();
+
+    virtual PlayerDecision MakeDecision(int currentBet, int pot, int minimumRaise) = 0;
+
+protected:
+    std::string _name;
+    int _chips;
+    std::vector<Card> _hand;
+    bool _folded;
 };
