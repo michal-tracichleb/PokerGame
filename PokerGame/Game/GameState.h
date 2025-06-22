ab8f4880b5d80667ccbed881ee2662c955a8ccd8
Michał Tracichleb <michal.tracichleb@gmail.com>
2025-06-22 13:18:36 +0200

    Add Phase enum to GameState for tracking round progress

--- PokerGame/Game/GameState.h
+++ PokerGame/Game/GameState.h
@@ -5,10 +5,21 @@
 
 class Card;
 
+enum class Phase : int8_t{
+    PreFlop,
+    Flop,
+    Turn,
+    River,
+    Showdown,
+
+    Size // Only for iteration
+};
+
 struct GameState {
     std::vector<Card> communityCards;
     int pot = 0;
     int currentBet = 0;
     int minimumRaise = 0;
     int activePlayerIndex = 0;
+    Phase phase = Phase::Size;
 };
